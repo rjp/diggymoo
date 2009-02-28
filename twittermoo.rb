38a2002a5cce3d2e3c2705bf4b000e7a7ee43102
Rob Partington <zimpenfish@gmail.com>
2009-02-28 13:37:58 +0000

    - rationalise the console output - ignore things we've printed already - don't bother checking for friends in the @ref - output things that are relevant to spread:bot_say - don't bother sleeping if we've ignore a message - don't bother cleaning our incoming spread queue

--- twittermoo.rb
+++ twittermoo.rb
@@ -14,42 +14,55 @@ twitter = Twitter::Base.new(config['email'], config['password'])
 
 already_seen = GDBM.new('/data/rjp/db/mootwits.db')
 
+puts "B fetching current timeline and ignoring"
 twitter.timeline(:friends).each do |s|
     sha1 = SHA1.hexdigest(s.text + s.user.name)
-#    already_seen[sha1] = "s"
+    already_seen[sha1] = "s"
 end
 
+puts "L entering main loop"
 loop {
+    puts "T fetching current timeline"
     twitter.timeline(:friends).reverse.each do |s|
 	    sha1 = SHA1.hexdigest(s.text + s.user.name)
-	    if already_seen[sha1].nil? then
+        status = already_seen[sha1]
+	    if status.nil? then
+            puts "N +/#{sha1} #{s.user.name} #{s.text[0..6]}..."
             ts = Time.parse(s.created_at)
             output = "<#{s.user.screen_name}> #{s.text} (#{ts.strftime('%Y%m%d %H%M%S')})"
             if s.text =~ /^@(\w+)\s/ then
                 puts "? #{$1}"
-                if twitter.friends.include?($1) then
+                if 1 then # twitter.friends.include?($1) then
     	            puts "+ #{output}"
-#                    sp.multicast(output, 'bot_say', Spread::RELIABLE_MESS)
+                    sp.multicast(output, 'bot_say', Spread::RELIABLE_MESS)
                 else
                     puts "- #{output}"
                 end
             else
     	        puts "+ #{output}"
-#                sp.multicast(output, 'bot_say', Spread::RELIABLE_MESS)
+                sp.multicast(output, 'bot_say', Spread::RELIABLE_MESS)
             end
-#            already_seen[sha1] = "s"
+            already_seen[sha1] = "p"
+            sleep 20
+        else
+            if status != 'p' then
+                puts "O #{status}/#{sha1} #{s.user.name} #{s.text[0..6]}..."
+            end
+            already_seen[sha1]='p'
 	    end
-        sleep 20
     end
 
 # clean out our incoming spread queue to avoid problems
-    loop do
-        messages = sp.poll
-        if messages > 0 then
-            junk = sp.receive
-        else
-            break
-        end
-    end
+#    messages = 1
+#    while messages > 0 do
+#        messages = sp.poll
+#        if messages > 0 then
+#            junk = sp.receive
+#        else
+#            break
+#        end
+#    end
+
+    puts "S #{Time.now}"
     sleep 300
 }
